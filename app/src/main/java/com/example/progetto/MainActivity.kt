15cdc41797a56f3233d9e5a7c7fc3dcf3f409aac
Roberto Mustillo <robertomustillo@gmail.com>
2022-06-22 12:15:18 +0200

    prima commit

--- app/src/main/java/com/example/progetto/MainActivity.kt
+++ app/src/main/java/com/example/progetto/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.progetto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.example.progetto.adapters.PcAdapter
 import com.example.progetto.adapters.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val tabLayout=findViewById<TabLayout>(R.id.tab_layout)
         val viewPager2=findViewById<ViewPager2>(R.id.view_pager_2)
@@ -24,6 +27,7 @@ class MainActivity: AppCompatActivity() {
             when(position){
                 0->{
                     tab.text="First"
+
                 }
                 1->{
                     tab.text="Second"
--- app/src/main/java/com/example/progetto/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/progetto/fragment/FirstFragment.kt
@@ -1,13 +1,23 @@
 package com.example.progetto.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.progetto.DetailedActivity
+import com.example.progetto.Pc
 import com.example.progetto.R
+import com.example.progetto.adapters.PcAdapter
 
-class FirstFragment: Fragment() {
+class FirstFragment: Fragment(){
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var pcList: ArrayList<Pc>
+    private lateinit var pcAdapter: PcAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +29,43 @@ class FirstFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        pcList = ArrayList()
+
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+        pcList.add(Pc(R.drawable.xbox, "game1"))
+        pcList.add(Pc(R.drawable.joker, "game2"))
+
+
+        pcAdapter = PcAdapter(pcList)
+        recyclerView.adapter = pcAdapter
+
+        pcAdapter.onItemClick = {
+            val intent = Intent(this.context, DetailedActivity::class.java)
+            intent.putExtra("pc", it)
+            startActivity(intent)
+        }
+
+    }
 }
